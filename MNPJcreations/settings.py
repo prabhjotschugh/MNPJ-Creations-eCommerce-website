1f1df1d831493e3b6cda0634eb4ed4cb1b333c80
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-08-01 18:52:53 +0530

    Update settings.py

--- MNPJcreations/settings.py
+++ MNPJcreations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm7757tfa4t=!#52_m1g6n8h(5_5-2@r8q$_3js367+0!4$d%-*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['prabhjotschugh-mnpj-creations-ecommerce.onrender.com']
 
 
 # Application definition
